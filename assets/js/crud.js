af0c58a3c1646863b3c6d8f16f0f952515f49398
dilenesg28 <cdilenes28@gmail.com>
2023-05-31 21:34:51 -0300

    finalização

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -1,9 +1,9 @@
 document.querySelector("#salvar").addEventListener("click", cadastrar)
 
-let atividades = []
+let contas = []
 
 window.addEventListener("load", () => {
-    atividades = JSON.parse(localStorage.getItem("atividades")) || []
+    contas = JSON.parse(localStorage.getItem("contas")) || []
     atualizar()
 })
 
@@ -13,37 +13,33 @@ window.addEventListener("load", () => {
 /****************************************************************** */
 document.querySelector("#busca").addEventListener("keyup", ()=> {
     let busca = document.querySelector("#busca").value
-    let atividadesFiltradas = atividades.filter((atividade) =>{
-        return atividade.titulo.toLowerCase().includes(busca.toLowerCase())
+    let contasFiltradas = contas.filter((conta) =>{
+        return conta.titulo.toLowerCase().includes(busca.toLowerCase())
     })
-    filtrar(atividadesFiltradas)
+    filtrar(contasFiltradas)
 })
 
-function filtrar(atividades){
-    document.querySelector("#atividades").innerHTML = ""
-    atividades.forEach((atividade) =>{
-        document.querySelector("#atividades").innerHTML 
-                    += criarCard(atividade)
+function filtrar(contas){
+    document.querySelector("#contas").innerHTML = ""
+    contas.forEach((conta) =>{
+        document.querySelector("#contas").innerHTML 
+                    += criarCard(conta)
     })
 }
 /*///////////////////////////////////////////////*/
 document.querySelector("#busca").addEventListener("clear", ()=> {
     let limpa = document.querySelector("#busca").value
-    /*let atividadesFiltradas = atividades.filter((atividade) =>{
-        return atividade.titulo.toLowerCase().includes(busca.toLowerCase())
-    })
-    filtrar(atividadesFiltradas)*/
     atualizar()
 })
 
 /****************************************************************** */
 document.querySelector("#filtrar_pendentes").addEventListener("click", () => {
-    let atividadesFiltradas = atividades.filter(atividade => !atividade.concluida)
-    filtrar(atividadesFiltradas)
+    let contasFiltradas = contas.filter(conta => !conta.concluida)
+    filtrar(contasFiltradas)
 })
 document.querySelector("#filtrar_concluidas").addEventListener("click", () => {
-    let atividadesFiltradas = atividades.filter(atividade => atividade.concluida)
-    filtrar(atividadesFiltradas)
+    let contasFiltradas = contas.filter(conta => conta.concluida)
+    filtrar(contasFiltradas)
 })
 document.querySelector("#limpar_filtro").addEventListener("click", () => {
     
@@ -51,42 +47,42 @@ document.querySelector("#limpar_filtro").addEventListener("click", () => {
 })
 /*---------------------------------------------*/ 
 function atualizar() {
-    localStorage.setItem("atividades", JSON.stringify(atividades))
-    document.querySelector("#atividades").innerHTML = ""
-    atividades.forEach(atividade =>
-        document.querySelector("#atividades").innerHTML += criarCard(atividade))     
+    localStorage.setItem("contas", JSON.stringify(contas))
+    document.querySelector("#contas").innerHTML = ""
+    contas.forEach(conta =>
+        document.querySelector("#contas").innerHTML += criarCard(conta))     
     
  
 }
 
 function filtrar(lista){
-    document.querySelector("#atividades").innerHTML = ""
-    lista.forEach(atividade => 
-        document.querySelector("#atividades").innerHTML += criarCard(atividade))
+    document.querySelector("#contas").innerHTML = ""
+    lista.forEach(conta => 
+        document.querySelector("#contas").innerHTML += criarCard(conta))
 }
 
 function cadastrar() {
     const titulo = document.querySelector("#titulo").value
-    const pontos = document.querySelector("#pontos").value
+    const litros = document.querySelector("#litros").value
     const dataI = document.querySelector("#dataI").value 
     const categoria = document.querySelector("#categoria").value
     const obs = document.querySelector("#obs").value
     const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
-    const atividade = { //JSON Java Script Object Notation
+    const conta = { //JSON Java Script Object Notation
         id:Date.now(),
         titulo,
-        pontos,
+        litros,
         dataI,
         categoria,
         obs,
         concluida: false
     }
 
-    if (!isValid(atividade.titulo, document.querySelector("#titulo"))) return
-    if (!isValid(atividade.categoria, document.querySelector("#categoria"))) return
-    if (!isValid(atividade.dataI, document.querySelector("#dataI"))) return
-    atividades.push(atividade)
+    if (!isValid(conta.titulo, document.querySelector("#titulo"))) return
+    if (!isValid(conta.categoria, document.querySelector("#categoria"))) return
+    if (!isValid(conta.dataI, document.querySelector("#dataI"))) return
+    contas.push(conta)
     atualizar()  
    
     modal.hide()
@@ -109,42 +105,39 @@ function isValid(valor, campo) {
 
 function apagar(id){
    
-    atividades = atividades.filter(atividade => atividade.id !== id)
+    contas = contas.filter(conta => conta.id !== id)
     atualizar()
 }
 
 function concluir(id){
-    let atividadeEncontrada = atividades.find(atividade => atividade.id == id)
-    atividadeEncontrada.concluida=true
+    let contaEncontrada = contas.find(conta => conta.id == id)
+    contaEncontrada.concluida=true
     atualizar()
 }
-
-
-
-function criarCard(atividade) {
-    let disabled = atividade.concluida ? "disabled" : ""
+function criarCard(conta) {
+    let disabled = conta.concluida ? "disabled" : ""
 
     const card = `
         <div class="col-lg-3 col-md-6 col-sm-12">
         <div class="card">
-            <div class="card-header"  style="background-color: #4682B4;" style="font-size: medium;">${atividade.titulo}                 
+            <div class="card-header"  style="background-color: #4682B4;" style="font-size: medium;">${conta.titulo}                 
             </div>
             <div class="input-group-addon"  >
-                <span class="glyphicon glyphicon-th" >${atividade.dataI}</span>
+                <span class="glyphicon glyphicon-th" >${conta.dataI}</span>
                 
             </div>
             <div class="card-body" style="background-color: #B0C4DE;"> 
-                <p class="card-text" >${atividade.obs}</p>                 
+                <p class="card-text" >${conta.obs}</p>                 
             </div>
             <div class="card-body" style="background-color: #B0C4DE;"> 
-                <span class="badge text-bg-warning">${atividade.categoria}</span>
-                <span class="badge text-bg-warning">${atividade.pontos}pt</span>
+                <span class="badge text-bg-warning">${conta.categoria}</span>
+                <span class="badge text-bg-warning">${conta.litros}litros</span>
             </div>       
             <div class="card-footer" style="background: #6495ED;">
-                <a href="#" onClick="concluir(${atividade.id})" class="btn btn-success ${disabled}" title="marcar como concluída">
+                <a href="#" onClick="concluir(${conta.id})" class="btn btn-success ${disabled}" title="marcar como concluída">
                     <i class="bi bi-check2"></i>
                 </a>
-                <a href="#" onClick="apagar(${atividade.id})" class="btn btn-danger" title="apagar tarefa">
+                <a href="#" onClick="apagar(${conta.id})" class="btn btn-danger" title="apagar tarefa">
                     <i class="bi bi-trash3"></i>
                 </a>
             </div> <!-- card footer -->
